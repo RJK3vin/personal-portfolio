fe3362902f6aafb045046ce223448a7eb67ffc67
Kevin Ritthaworn <rj.ritthaworn@icloud.com>
2025-06-14 00:58:06 -0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="h-screen flex items-center justify-center bg-gradient-to-br from-blue-500 to-purple-600 text-white text-4xl font-bold">
+        Kevin's Portfolio 🚀
+      </div>
+    </>
+  )
+}
+
+export default App
