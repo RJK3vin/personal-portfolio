d8adbf5d593e66ea2a278740bf3687265ce05fb2
Kevin Ritthaworn <rj.ritthaworn@icloud.com>
2025-06-16 02:11:11 -0400

    adjusted css

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -7,13 +7,13 @@ interface ProjectCardProps {
 export default function ProjectCard({ title, description, link}: ProjectCardProps) {
     return (
         <div className = "bg-gray-800 p-6 rounded-xl shadow-md mb-6">
-            <h2 className = "text-2xl font-semibold text-white">{title}</h2>
-            <p className = "mt-2 text-gray-300">{description}</p>
+            <h2 className = "text-2xl font-semibold text-white mb-2">{title}</h2>
+            <p className = "text-gray-300 mb-4">{description}</p>
             <a
                 href={link}
                 target="_blank"
                 rel="noopener noreferrer"
-                className = "inlinde-block mt-4 bg-white text-black px-4 py-2 rounded-lg font-semibold hover:bg-gray-200 transition"
+                className = "inlinde-block bg-white text-black px-4 py-2 rounded-lg font-semibold hover:bg-gray-200 transition"
             >
                 View Project →
             </a>
