dacf02278de489fd6ff788640893c990b6cb08fb
Kevin Ritthaworn <rj.ritthaworn@icloud.com>
2025-06-24 02:41:08 -0400

    added barbershop project

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -5,17 +5,23 @@ export interface Project {
     link: string
 }
 
-export const projects: Project[]=  [
+export const projects: Project[] = [
     {
         id: 1,
         title: "EasyGradPath",
         description: "A smart academic planning tool that helps students create personalized graduation paths using priority scheduling logic.",
         link: "https://chill-helicopter-d36.notion.site/EasyGradPlan-Your-Easiest-Path-to-Graduation-21191add595480f2af17c6049ef657cc"
     },
-    {  
+    {
         id: 2,
         title: "Wellness Detailing",
         description: "A custom-built website for a local car wash business, including service listings, a quote form, embedded map, and customer testimonials to attract new clients",
         link: "placeholderlink"
-    }
+    },
+    {
+        id: 3,
+        title: "Kevin Cuts Barbershop Website",
+        description: "A fictional barbershop site built with React, TypeScript, Tailwind CSS, React Router, and Framer Motion. Includes booking form, gallery, and full-page animations.",
+        link: "https://kevin-cuts.vercel.app/"
+    },
 ]
